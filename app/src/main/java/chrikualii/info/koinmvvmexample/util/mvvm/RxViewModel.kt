3caba9831d8fe6773803a6dedc205d10f23d198b
Ali Rahmat Ismail <chiriku.ali.rhmat@gmail.com>
2019-05-05 16:12:07 +0700

    modify rxViewmodel,viewmodelstate

--- app/src/main/java/chrikualii/info/koinmvvmexample/util/mvvm/RxViewModel.kt
+++ app/src/main/java/chrikualii/info/koinmvvmexample/util/mvvm/RxViewModel.kt
@@ -17,6 +17,9 @@ abstract class RxViewModel : ViewModel() {
     protected abstract val TAG: String
     private val disposable = CompositeDisposable()
     protected val _states = MutableLiveData<ViewModelState>()
+    private val loading = Loading(false)
+    private var failed = Failed()
+    private var success = Success()
     val states: LiveData<ViewModelState>
         get() = _states
 
@@ -29,5 +32,38 @@ abstract class RxViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         disposable.clear()
+    }
+
+    fun showLoading(isLoading: Boolean){
+        loading.isLoading = isLoading
+        _states.value = loading
+    }
+
+    fun showErrorMessage(message: String?=null,t: Throwable?=null){
+        if(message!= null){
+            failed.error = message
+            _states.value = failed
+        }
+
+        if (t !=null){
+            when (t){
+
+                is java.net.UnknownHostException -> {
+                    failed.error = "No Internet Connection"
+                    _states.value = failed
+                }
+
+                else ->{
+                    failed.error = t.message
+                    _states.value = failed
+                }
+            }
+        }
+
+    }
+
+    fun showSuccessMessage(message:String?=null){
+        success.message = message
+        _states.value = success
     }
 }
--- app/src/main/java/chrikualii/info/koinmvvmexample/util/mvvm/ViewModelState.kt
+++ app/src/main/java/chrikualii/info/koinmvvmexample/util/mvvm/ViewModelState.kt
@@ -4,7 +4,7 @@ package chrikualii.info.koinmvvmexample.util.mvvm
  * Created by chirikualii on 11/12/18
  * github.com/chirikualii
  */
-open class ViewModelState
-
-data class Loading(val isLoading: Boolean) : ViewModelState()
-data class Failed(val error: String?) : ViewModelState()
+sealed class ViewModelState
+data class Success(var message: String?="") :ViewModelState()
+data class Loading(var isLoading: Boolean) : ViewModelState()
+data class Failed(var error: String?="") : ViewModelState()
